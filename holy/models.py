a55b24908922bdf323ad300d1513f0ba2eb3a138
sun0511 <sun0511777@gmail.com>
2019-10-24 01:38:42 +0900

    final

--- holy/models.py
+++ holy/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    image = models.ImageField(blank=True)
+    text = models.TextField()
+    created_At = models.DateTimeField(auto_now_add=True)
+    updated_At = models.DateTimeField(auto_now=True)
